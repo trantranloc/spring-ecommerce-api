e76a8a6f8963a072ad9596714c28bac09e76f530
Loc Tran Tran <tran26122003@gmail.com>
2024-12-19 20:24:23 +0700

    Authenticated

--- src/main/java/com/spring/springecommerceapi/controller/UserController.java
+++ src/main/java/com/spring/springecommerceapi/controller/UserController.java
@@ -4,7 +4,10 @@ import com.spring.springecommerceapi.exception.ErrorCode;
 import com.spring.springecommerceapi.model.User;
 import com.spring.springecommerceapi.dto.request.ApiRequest;
 import com.spring.springecommerceapi.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +16,7 @@ import java.util.List;
 @RequestMapping("/api/v1/users")
 public class UserController extends BaseController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -22,6 +26,12 @@ public class UserController extends BaseController {
 
     @GetMapping
     public ResponseEntity<ApiRequest<List<User>>> getAllUser() {
+        var securityContextHolder = SecurityContextHolder.getContext().getAuthentication();
+        log.info(securityContextHolder.getName());
+        securityContextHolder.getAuthorities().forEach(
+                grantedAuthority ->
+                        log.info(grantedAuthority.getAuthority())
+        );
         List<User> users = userService.getAllUsers();
         return createApiResponse(ErrorCode.SUCCESS,users);
     }
